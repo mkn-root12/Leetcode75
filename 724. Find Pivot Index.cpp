649607fe782097c9c765e11d5d6e0c4606c5617a
mkn-root12 <75845290+mkn-root12@users.noreply.github.com>
2022-08-01 10:02:41 +0530

    Create 724. Find Pivot Index.cpp

---
+++ 724. Find Pivot Index.cpp
@@ -0,0 +1,69 @@
+Given an array of integers nums, calculate the pivot index of this array.
+
+The pivot index is the index where the sum of all the numbers strictly to the left of the index is equal to the sum of all the numbers strictly to the index's right.
+
+If the index is on the left edge of the array, then the left sum is 0 because there are no elements to the left. This also applies to the right edge of the array.
+
+Return the leftmost pivot index. If no such index exists, return -1.
+
+ 
+
+Example 1:
+
+Input: nums = [1,7,3,6,5,6]
+Output: 3
+Explanation:
+The pivot index is 3.
+Left sum = nums[0] + nums[1] + nums[2] = 1 + 7 + 3 = 11
+Right sum = nums[4] + nums[5] = 5 + 6 = 11
+Example 2:
+
+Input: nums = [1,2,3]
+Output: -1
+Explanation:
+There is no index that satisfies the conditions in the problem statement.
+Example 3:
+
+Input: nums = [2,1,-1]
+Output: 0
+Explanation:
+The pivot index is 0.
+Left sum = 0 (no elements to the left of index 0)
+Right sum = nums[1] + nums[2] = 1 + -1 = 0
+ 
+
+Constraints:
+
+1 <= nums.length <= 104
+-1000 <= nums[i] <= 1000
+  
+  #SOLUTION
+  /**
+Approach #1: Prefix Sum [Accepted]
+**/
+
+/**
+Complexity Analysis
+Time Complexity: O(N)O(N), where NN is the length of nums.
+Space Complexity: O(1)O(1), the space used by leftsum and S.
+**/
+
+//Runtime: 28 ms, faster than 98.42% of C++ online submissions for Find Pivot Index.
+//Memory Usage: 9.8 MB, less than 100.00% of C++ online submissions for Find Pivot Index.
+
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        int allsum = accumulate(nums.begin(), nums.end(), 0);
+        int cumsum = 0;
+        
+        for(int i = 0; i < nums.size(); i++){
+            if(cumsum*2 == (allsum-nums[i])){
+                return i;
+            }
+            cumsum += nums[i];
+        }
+        
+        return -1;
+    }
+};
