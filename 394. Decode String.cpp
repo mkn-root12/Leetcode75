f854d57dc0e0883dc1f3b3e2c3fee7686bfc0329
mkn-root12 <75845290+mkn-root12@users.noreply.github.com>
2022-08-15 13:11:57 +0530

    Create 394. Decode String.cpp

---
+++ 394. Decode String.cpp
@@ -0,0 +1,76 @@
+
+Given an encoded string, return its decoded string.
+
+The encoding rule is: k[encoded_string], where the encoded_string inside the square brackets is being repeated exactly k times. Note that k is guaranteed to be a positive integer.
+
+You may assume that the input string is always valid; there are no extra white spaces, square brackets are well-formed, etc. Furthermore, you may assume that the original data does not contain any digits and that digits are only for those repeat numbers, k. For example, there will not be input like 3a or 2[4].
+
+The test cases are generated so that the length of the output will never exceed 105.
+
+ 
+
+Example 1:
+
+Input: s = "3[a]2[bc]"
+Output: "aaabcbc"
+Example 2:
+
+Input: s = "3[a2[c]]"
+Output: "accaccacc"
+Example 3:
+
+Input: s = "2[abc]3[cd]ef"
+Output: "abcabccdcdcdef"
+ 
+
+Constraints:
+
+1 <= s.length <= 30
+s consists of lowercase English letters, digits, and square brackets '[]'.
+s is guaranteed to be a valid input.
+All the integers in s are in the range [1, 300].
+  
+  
+  
+  class Solution {
+public:
+    string decodeString(string s) {
+        stack<string> stks;
+        stack<int> stki;
+        
+        string curs = "";
+        int curi = 0;
+        
+        for(char& c : s){
+            if(c == '['){
+                stks.push(curs);
+                stki.push(curi);
+                curs = "";
+                curi = 0;
+            }else if(c == ']'){
+                /*
+                between [ and ], there is only chars,
+                so here we only use curs, not curi
+                
+                we can see the string until ] as:
+                pres + prei[curs],
+                and we need to set curs as the parsed string
+                */
+                string pres = stks.top(); stks.pop();
+                int prei = stki.top(); stki.pop();
+                
+                string tmp;
+                for(int i = 0; i < prei; ++i){
+                    tmp += curs;
+                }
+                curs = pres + tmp;
+            }else if(isdigit(c)){
+                curi = curi*10 + (c-'0');
+            }else{
+                curs += c;
+            }
+        }
+        
+        return curs;
+    }
+};
